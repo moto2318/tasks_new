ccd87f82a215a1762bff26551d4c9b66cf5bc8fe
モト2318 <suigekka0101@gmail.com>
2023-11-20 22:07:53 +0900

    アプリケーション完成　３章

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get 'lists/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'top'  => 'homes#top'
+  get 'lists/:id' => 'lists#show', as: 'list'
 end
